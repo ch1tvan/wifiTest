96895ad018a2aca6edd3f9aebe175fc0ed70d961
Chitvan Killawala <cvk9@miami.edu>
2022-11-11 17:10:34 -0500

    Started building in case the other one fails

---
+++ wifiTest.ino
@@ -0,0 +1,19 @@
+//    Communication Definitions
+#define IPServer "192.168.1.80"
+#define UDP_PORT 7394
+#define NW_NAME "FCI_No_Internet_2.4"
+#define PWD  "57381909"
+
+// Variables used in communication
+WiFiUDP _udp;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
