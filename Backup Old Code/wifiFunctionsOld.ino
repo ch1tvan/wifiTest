e51712380c0e95c19e30977c96a892c20e273bc7
Chitvan Killawala <cvk9@miami.edu>
2023-01-27 18:53:39 -0500

    Basic Communication Working

--- Backup Old Code/wifiFunctionsOld.ino
+++ Backup Old Code/wifiFunctionsOld.ino
@@ -1,4 +1,4 @@
-
+bool wifiCon = false;
 
 void scan4Wifi() {
   Serial.println("scan start");
@@ -13,60 +13,63 @@ void scan4Wifi() {
     Serial.println(" networks found");
     for (int i = 0; i < n; ++i) {
       // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
       Serial.print(WiFi.SSID(i));
       Serial.print(" (");
       Serial.print(WiFi.RSSI(i));
       Serial.print(")");
       Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
       if(WiFi.SSID(i) == NW_NAME) {
         WiFi.disconnect();
         WiFi.begin(NW_NAME,PWD);
         Serial.print("Connecting to ");
         Serial.println(WiFi.SSID(i));
-        delay(1000); // Wait for connection
-        return;
+        delay(5000); // Wait for connection
       }
-      delay(10);
     }
+    // WiFi.begin(NW_NAME,PWD);
+    // connect2Network();
   }
   Serial.println("");
+
+  // Wait a bit before scanning again
+  // delay(5000);
 }
 
 
-bool UDP_handshake() {
+bool UDP_handshake(int localPort) {
   if(wifiCon) {
     return(wifiCon);
   }
-  _udp.begin(UDP_PORT);
+  _udp.begin(localPort);
   // Put the handshake code here
-  send2Server("Hello");
-  wifiCon = true;
-  return(wifiCon);
+  strcpy(replyBuffer,"Hello");
+  _udp.beginPacket(_udp.remoteIP(), localPort);
+  _udp.write((uint8_t *)replyBuffer,5);
+  _udp.endPacket();
+  return(true);
 }
 
 void readFromUDP() {
-  _udp.parsePacket();
-  int packetSize = _udp.read(packetBuffer, 255);
-  Serial.println(packetSize);
+  int packetSize = _udp.parsePacket();
   if (packetSize) {
-    packetBuffer[packetSize] = 0;
     Serial.print("Received packet of size ");
     Serial.println(packetSize);
-    // Serial.print("From ");
-    // Serial.print(_udp.remoteIP());
-    // Serial.print(", port ");
-    // Serial.println(_udp.remotePort());
-    Serial.print("Contents: ");
+    Serial.print("From ");
+    Serial.print(_udp.remoteIP());
+    Serial.print(", port ");
+    Serial.println(_udp.remotePort());
+    int len = _udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Contents:");
     Serial.println(packetBuffer);
-    send2Server("Hello Back");
   }
 }
 
-void send2Server(char * str) {
-  _udp.beginPacket(IPServer, UDP_PORT);
-  _udp.write((uint8_t *)str,strlen(str));
-  _udp.endPacket();
-}
 // void connect2Network() {
 //   Serial.println("Trying to connect to current network");
 //   WiFi.begin(NW_NAME,PWD);
--- Backup Old Code/wifiTestOld.ino
+++ Backup Old Code/wifiTestOld.ino
@@ -2,7 +2,7 @@
 #include <WiFi.h>
 
 //    Communication Definitions
-#define IPServer "192.168.1.150"
+#define IPServer "192.168.1.102"
 #define UDP_PORT 7394
 #define NW_NAME "FCI_No_Internet_2.4"
 // #define NW_NAME "FCI_No_Internet_NotExist"
@@ -11,8 +11,7 @@
 // Variables used in communication
 WiFiUDP _udp;
 char packetBuffer[255];
-int wifiConnectionTime = 2000, prevMS = 0;
-bool wifiCon = false;
+char replyBuffer[255];
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,18 +25,22 @@ void setup() {
 }
 
 void loop() {
-  int currMS = millis() % wifiConnectionTime;
+  static int prevMS = 0;
+  int currMS = millis()%5000;
   if(WiFi.status() == WL_CONNECTED) {
     Serial.print("Wifi local IP is ");
     Serial.println(WiFi.localIP());
-    if(UDP_handshake()) {
+    if(UDP_handshake(UDP_PORT)) {
       readFromUDP();
-      // send2UDP();
+      char replyPrefix [] = "Received: \0";
+      strcpy(replyBuffer, replyPrefix);
+      Serial.println(replyBuffer);
+      strncat(replyBuffer,packetBuffer,255-strlen(replyPrefix));
+      Serial.println(replyBuffer);
     }
-    delay(500);
+    delay(1000);
   }
   else if(prevMS>currMS) {
-    wifiCon = false;
     scan4Wifi();
     // connect2Network();
   }
